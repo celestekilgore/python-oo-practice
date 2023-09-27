46683644d9fd61a960c746844bb9d8811bf05629
Celeste Kilgore <celestekilgore@gmail.com>
2023-09-26 23:11:44 -0400

    finished exercise

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Make a new instance of SerialGenerator"""
+
+        self.start = start
+        self.tracker = start #often called next
+        # can combine since they equal the same thing
+
+    def __repr__(self):
+        return f"<Serial Generator start={self.start}"
+
+    def generate(self):
+        """Update serial tracker, return current serial number"""
+
+        self.tracker += 1
+        return self.tracker - 1
+
+    def reset(self):
+        """Reset the serial tracker to the initial start number"""
+
+        self.tracker = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,39 @@
+from random import choice
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a file."""
+
+    def __init__(self,file_path):
+        """Makes a new instance of WordFinder given a file path"""
+
+        print("inside parent __init__")
+        self.file_path = file_path
+        self.words = self.get_file_data()
+        print(f"{len(self.words)} words read")
+
+    def __repr__(self):
+        return f"<WordFinder file path = {self.file_path}"
+
+    def get_file_data(self):
+        """Reads the instance's file, returns a list of each line in the file"""
+
+        print("inside parent get_file_data")
+        with open(self.file_path,"r") as file:
+            words_list = [line.strip() for line in file]
+        return words_list
+
+    def random(self):
+        """returns a random element from instance's words list"""
+
+        return choice(self.words)
+
+class RandomWordFinder(WordFinder):
+    """Finds random words from a file. Adds clean_file method"""
+
+    def get_file_data(self): #make method name the same as parent for polymorphism
+        """Iterates over instances word list and removes extra whitespace and #s"""
+
+        print("inside child get_file_data")
+        lst = [word for word in super().get_file_data() if word != "" and not word.startswith("#")]
+        return lst
+
